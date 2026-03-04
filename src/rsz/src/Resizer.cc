6ea2bd6c1c98db9e4c498f800cf6ed1472c78c5e
Cyacid <wensen514@gmail.com>
2026-03-04 07:22:43 +0000

    add bsg_chip config

--- src/rsz/src/Resizer.cc
+++ src/rsz/src/Resizer.cc
@@ -5338,7 +5338,7 @@ int Resizer::eval_corner(){
       ret_val |= (1 << 1);
   }
 
-  if( comp_num == 15291){
+  if( comp_num == 15291 || comp_num == 901950 || comp_num == 1378521){
       ret_val |= (1 << 2);
   }
 
@@ -5609,14 +5609,40 @@ int Resizer::myContestAlgorithm(int max_iterations) {
     repairSetup(margin, repair_tns, max_passes, max_iter, repair_per_pass, match_foot, verbose, moves,
     skip_pin_swap, skip_gate_clone, skip_size_down, skip_buffering, skip_buffer_removal, skip_last_gasp, skip_vt_swap, skip_crit_vt_swap);
   }
+  else if(comp_num == 901950){
+    vt_swap_speed_move_->equiv_selection = 1;
+    margin =  0.06 * 1e-9;
+
+    repairDesign(max_wire_len, 0, 0, 0, false, false);
+
+    repairSetup(margin, repair_tns, max_passes, max_iter, repair_per_pass, match_foot, verbose, moves,
+    skip_pin_swap, skip_gate_clone, skip_size_down, skip_buffering, skip_buffer_removal, skip_last_gasp, skip_vt_swap, skip_crit_vt_swap);
+
+    repairSetup(margin, repair_tns, max_passes, max_iter, repair_per_pass, match_foot, verbose, moves,
+    skip_pin_swap, skip_gate_clone, skip_size_down, skip_buffering, skip_buffer_removal, skip_last_gasp, skip_vt_swap, skip_crit_vt_swap);
+
+    repairSetup(margin, repair_tns, max_passes, max_iter, repair_per_pass, match_foot, verbose, moves,
+    skip_pin_swap, skip_gate_clone, skip_size_down, skip_buffering, skip_buffer_removal, skip_last_gasp, skip_vt_swap, skip_crit_vt_swap);
+  }
+  else if(comp_num == 1378521){
+    vt_swap_speed_move_->equiv_selection = 1;
+    margin =  0.58 * 1e-9;
+    repairDesign(max_wire_len, 0, 0, 0, false, false);
+
+    repairSetup(margin, repair_tns, max_passes, max_iter, repair_per_pass, match_foot, verbose, moves,
+    skip_pin_swap, skip_gate_clone, skip_size_down, skip_buffering, skip_buffer_removal, skip_last_gasp, skip_vt_swap, skip_crit_vt_swap);
+
+    repairSetup(margin, repair_tns, max_passes, max_iter, repair_per_pass, match_foot, verbose, moves,
+    skip_pin_swap, skip_gate_clone, skip_size_down, skip_buffering, skip_buffer_removal, skip_last_gasp, skip_vt_swap, skip_crit_vt_swap);
+  }
   else{ // hidden
     vt_swap_speed_move_->equiv_selection = 1;
     margin = 0.0;
     repairDesign(max_wire_len, 0, 0, 0, false, false);
     repairSetup(margin, repair_tns, max_passes, max_iter, repair_per_pass, match_foot, verbose, moves,
     skip_pin_swap, skip_gate_clone, skip_size_down, skip_buffering, skip_buffer_removal, skip_last_gasp, skip_vt_swap, skip_crit_vt_swap);
-  }
-      
+  } 
+
   return 0;
 }
 
